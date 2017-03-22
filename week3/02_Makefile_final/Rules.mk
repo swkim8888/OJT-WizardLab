83c0857b2b1d771b7f1282a71fc51dd0946a5c91
swkim8888 <swkim8888@naver.com>
2017-03-22 10:20:00 +0900

    make autoconf folder and content

---
+++ week3/02_Makefile_final/Rules.mk
@@ -0,0 +1,12 @@
+compile:
+	@for dir in $(SUBDIRS); do \
+	make -C $$dir || exit $?; \
+	done
+
+
+clean:
+	@for dir in $(SUBDIRS); do \
+	make -C $$dir clean; \
+	done
+	rm -rf *.o *.i *.s $(TARGET)
+
